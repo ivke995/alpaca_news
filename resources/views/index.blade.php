833e3a58baed7a022a2e08d80156074c651736d4
daniloStupar <danilostupar@hotmail.com>
2022-11-02 07:26:17 +0100

    frontend changed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
-    <title>Home</title>
-
-    <style>
-        .card-text {
-            text-align: left;
-        }
-    </style>
-</head>
-<body>
-
+@extends('layout')
+@section('content')
 <div class="container">
     <div class="row">
         @foreach($sources as $source)
@@ -41,7 +23,6 @@
         @endforeach
     </div>
 </div>
+@endsection
 
 
-</body>
-</html>
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,46 @@
+
+
+    <nav class="navbar fixed-top navbar-expand-lg bg-light ">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Sport</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Politika</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Ekonomija</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Kultura</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Zivot i stil</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Auto-moto</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Nauka</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Vijesti</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Magazin</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
