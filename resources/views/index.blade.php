196b0b3354ee819078b18ed2990cfda1ed051118
ivke995 <ivanivic842@gmail.com>
2022-11-10 14:07:39 +0100

    slug except id added for url

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,6 @@
 
 
 <div class="container">
-    <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="subscriber_email" name="subscriber_email" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
     <div class="row">
         @foreach($sources as $source)
             <div class="col-12 col-md-6 my-4 margin0">
