ee8e2decb73cec7b0e9df120d18a55df884ad7b3
ivke995 <ivanivic842@gmail.com>
2022-11-10 14:15:59 +0100

    smallArticle slug except id

--- resources/views/smallArticle.blade.php
+++ resources/views/smallArticle.blade.php
@@ -1,6 +1,6 @@
 
 <div class='smallArticle forHorScroll shadow-lg' >
-    <a href="{{ route('show', $article->id) }}">
+    <a href="{{ route('show', $article->slug) }}">
     <div class="smallArtTitle"><b>{{ $article->title }}</b></div>
     <div class="smallArtSrc"><b>{{ $article->source->name }}</b></div>
     <img src="{{ $article->image_url }}" class="img-fluid widthHor"/></a>
